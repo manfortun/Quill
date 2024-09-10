3b0b764efa4c6c0fefbf8c136aeb5a5da95697c3
manfortun <m.fortun@arcanys.com>
2024-09-10 09:35:28 +0800

    Removing test error

--- Quill.Server/Controllers/ContentsController.cs
+++ Quill.Server/Controllers/ContentsController.cs
@@ -18,7 +18,6 @@ public class ContentsController : ControllerBase
         _repository = repository;
         _tocService = tocService;
         _cacheService = cacheService;
-        this will add error on CI
     }
 
     [HttpGet("{identifier}")]
